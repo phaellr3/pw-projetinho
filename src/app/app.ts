5c253384cffd003c464155ea5714043ffcd06b2e
phaellr3 <r3campanher@gmail.com>
2026-03-03 15:20:53 -0300

    Aula dia 03/03

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,24 +10,30 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('pw3-hello-world');
   frases: any = [
-    ""
+    "Marion", "da", "MB", "Marion da MB !!!"
   ];
+  mensagem = this.frases;
   imagem = "biscoito.png";
   fechado: boolean = true;
+  div: boolean = true;
 
   abrir (){
-    if( this.fechado){
+    if(this.fechado){
       this.fechado = false;
       this.imagem = "quebrado.png"
-
+      this.mensagem = this.frases[Math.floor(Math.random() * this.frases.length)];
+      this.div = false
     }
-    
+
   }
   limpar(){
     if(!this.fechado){
       this.fechado = true
       this.imagem = "biscoito.png"
+      this.div = true
     }
   }
+
+
 
 }
